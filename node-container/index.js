d72786f58b98854dbcb3e7c0a332861b83db162a
JoaoVitor615 <jvpeniza@gmail.com>
2024-07-17 22:12:05 -0300

    feat: creating an express example

--- node-container/index.js
+++ node-container/index.js
@@ -1,1 +1,11 @@
-console.log('\nhello world!\n')
+const express = require('express')
+const app = express() 
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Running in port ${port}`)
+})
